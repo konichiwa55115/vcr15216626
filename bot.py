df2a0449f62dfc17a955f9c9ba62a0d387d15a49
konichiwa55115 <121526988+konichiwa55115@users.noreply.github.com>
2023-04-13 17:15:13 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,38 +4,13 @@ bot = Client(
     "myfirs",
     api_id=17983098,
     api_hash="ee28199396e0925f1f44d945ac174f64",
-    bot_token="5782497998:AAGQaYV5w_Rw1ZSBhApXgV763_g8wf_PZEw"
+    bot_token="6026272284:AAG8qBeaZc5xjYmYfMq1YWsKimoK0D0ap_0"
 )
 @bot.on_message(filters.command('start') & filters.private)
 def command1(bot,message):
-    bot.send_message(message.chat.id, " السلام عليكم أنا بوت تفريغ صوتيات , فقط أرسل الصوتية هنا\n\n  لبقية البوتات هنا \n\n https://t.me/ibnAlQyyim/1120 ",disable_web_page_preview=True)
-    
-
-    
-@bot.on_message(filters.private & filters.incoming & filters.audio )
-def _telegram_file(client, message):
-  user_id = message.from_user.id
-  sent_message = message.reply_text('جار التفريغ', quote=True)
-  file = message.audio
-  file_path = message.download(file_name="entry")
-
-    # Execute speech.py script with entry file
-  subprocess.call(['python3', 'speech.py', 'RK3ETXWBJQSMO262RXPAIXFSG6NH3QRH', "./downloads/entry" , 'transcription.txt'])
-    # Upload transcription file to user
-  with open('transcription.txt', 'rb') as f:
-        bot.send_document(message.chat.id, f)
- 
-@bot.on_message(filters.private & filters.incoming & filters.voice )
-def _telegram_file(client, message):
-  user_id = message.from_user.id
-  sent_message = message.reply_text('جار التفريغ', quote=True)
-  file = message.voice
-  file_path = message.download(file_name="entry")
-
-    # Execute speech.py script with entry file
-  subprocess.call(['python3', 'speech.py', 'RK3ETXWBJQSMO262RXPAIXFSG6NH3QRH', "./downloads/entry" , 'transcription.txt'])
-    # Upload transcription file to user
-  with open('transcription.txt', 'rb') as f:
-        bot.send_document(message.chat.id, f)
+    bot.send_message(message.chat.id, "جار التسجيل")
+    subprocess.call(['python3', 'main.py'])
+    subprocess.call(['sudo', 'zip', 'gawr','-r','Gawr])
+    subprocess.call(['rclone', 'copy', 'gawr.zip' , '"karim":"hamma262662"'])
 
 bot.run()
