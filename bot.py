2379bd2fec6b96cd16d598262e636707d2f38ded
konichiwa55115 <121526988+konichiwa55115@users.noreply.github.com>
2023-04-13 17:49:30 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -12,10 +12,11 @@ def command1(bot,message):
 @bot.on_message(filters.private & filters.incoming & filters.text )
 def _telegram_file(client, message):
     user_id = message.from_user.id
+    input_text = message_text.split()[1]
     sent_message = message.reply_text('جار التسجيل', quote=True)
     subprocess.call(['python3', 'main.py'])
     subprocess.call(['sudo', 'zip', 'gawr','-r','Gawr])
-    subprocess.call(['rclone', 'copy', 'gawr.zip' , 'karim',':',text])
+    subprocess.call(['rclone', 'copy', 'gawr.zip' , 'karim',':', input_text])
     sent_message = message.reply_text('ستجد التسجيل الخاص بك على المنصة التي حددت', quote=True)
 
 bot.run()
